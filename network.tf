f79e302ba50fa30eefee270c6b7311a10d97c18b
Wade Davis <wadavis@costco.com>
2022-10-03 20:16:42 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/workshop-wadavis/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "Gaurav"
+  resource_group_name = "myresourcegroup"
 }
